e14549c691e70612e1181221f547edb765353d7a
Elisabeth Jäntti <Elisabeth.jantti@hotmail.com>
2026-06-27 18:42:26 +0200

    Lanserar Mrs J Hub Version 2

--- app.py
+++ app.py
@@ -17,5 +17,20 @@ def kanslotracker():
 def pdf_journaling():
     return render_template("pdf_journaling.html")
 
+# Vad händer?
+@app.route("/vad_hander")
+def vad_hander():
+    return render_template("vad_hander.html")
+
+    # Hur hjälper jag mig själv?
+@app.route("/hur_hjalper")
+def hur_hjalper():
+    return render_template("hur_hjalper.html")
+
+    # nervsystemscheck
+@app.route("/nervsystem")
+def nervsystem():
+    return render_template("nervsystem.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
